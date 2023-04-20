634d72572054613e088ded4f772e7cf034db0a94
Senyksia <senyksia@gmail.com>
2023-04-20 11:41:46 +1000

    Add version to compiled .dll

---
+++ InfiniteFriends/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+
+[assembly: AssemblyVersion(InfiniteFriends.PluginInfo.PLUGIN_VERSION)]
+[assembly: AssemblyFileVersion(InfiniteFriends.PluginInfo.PLUGIN_VERSION)]
+[assembly: AssemblyInformationalVersion(InfiniteFriends.PluginInfo.PLUGIN_VERSION)]
