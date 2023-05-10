62ee03637bceb8d085a73e769e2b28182d127d83
Senyksia <senyksia@gmail.com>
2023-05-10 14:37:56 +1000

    Convert customization panel layout to scaleable grid

--- InfiniteFriends/Patches/CustomizationMenu.cs
+++ InfiniteFriends/Patches/CustomizationMenu.cs
@@ -7,7 +7,63 @@ using UnityEngine.UI;
 namespace InfiniteFriends.Patches
 {
     /*
-     * The following patches statically increase the number of customization panels and preview spiders.
+     * Acts like a GridLayoutGroup, but uses localScale to shrink and fit more children.
+     */
+
+    public class ScalingGridLayoutGroup : LayoutGroup
+    {
+        public float aspectRatio = 1f;
+        public Vector3 defaultScale = Vector3.one;
+
+        public override void CalculateLayoutInputHorizontal()
+        {
+            base.CalculateLayoutInputHorizontal();
+        }
+
+        public override void CalculateLayoutInputVertical()
+        {
+        }
+
+        public override void SetLayoutHorizontal()
+        {
+            SetCells();
+        }
+
+        public override void SetLayoutVertical()
+        {
+        }
+
+        private void SetCells()
+        {
+            int count = rectChildren.Count;
+            if (count < 1) return;
+
+            float selfWidth = rectTransform.rect.width;
+            float selfHeight = rectTransform.rect.height;
+            int rows = Mathf.CeilToInt(Mathf.Sqrt(count*selfHeight*0.97f*aspectRatio/selfWidth));
+            int cols = Mathf.CeilToInt((float)count/rows);
+            float cellWidth = selfWidth/cols;
+            float cellHeight = selfHeight/rows;
+
+            // Position and scale children horizontally and vertically
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    int i = cols*row + col;
+                    if (i >= count) break;
+
+                    float shiftUp = (rows-1) * cellHeight/2f; // Fix vertical axis always positioning from the centre
+                    rectChildren[i].localScale = defaultScale / Mathf.Pow(2, rows-1);
+                    SetChildAlongAxis(rectChildren[i], 0, (col+0.5f) * cellWidth);
+                    SetChildAlongAxis(rectChildren[i], 1, (row)      * cellHeight - shiftUp);
+                }
+            }
+        }
+    }
+
+    /*
+     * Statically increase the number of customization panels.
      * Ideally this would be done dynamically, but the network manager is uncooperative after initialising its children.
      */
 
@@ -18,6 +74,15 @@ namespace InfiniteFriends.Patches
         static bool Prefix(ref CustomizationPanel[] ___customizationPanels)
         {
             List<CustomizationPanel> panels = ___customizationPanels.ToList();
+
+            // Replace HorizontalLayoutGroup with a ScalingGridLayoutGroup
+            const float panelWidth  = 229f;
+            const float panelHeight = 436.95f;
+            GameObject layoutGroup = panels[0].transform.parent.gameObject;
+            HorizontalLayoutGroup.DestroyImmediate(layoutGroup.GetComponent<HorizontalLayoutGroup>());
+            ScalingGridLayoutGroup grid = layoutGroup.AddComponent<ScalingGridLayoutGroup>();
+            grid.aspectRatio = panelWidth/panelHeight;
+            grid.defaultScale = panels[0].transform.localScale;
 
             // Extend the number of customization panels
             while (panels.Count < InfiniteFriends.MaxPlayerHardCap)
@@ -41,47 +106,6 @@ namespace InfiniteFriends.Patches
             }
 
             ___customizationPanels = panels.ToArray();
-            return true;
-        }
-    }
-
-    [HarmonyPatch(typeof(CustomizationPreviewManager), "Awake")]
-    class CustomizationPreviewManager_Patch_Awake
-    {
-        [HarmonyPrefix]
-        static bool Prefix(ref List<SpiderCustomizer> ___previewCustomizers, ref List<Animator> ___previewAnimators)
-        {
-            // Widen texture atlas
-            Camera previewCamera = ___previewCustomizers[0].transform.parent.parent.GetComponentInChildren<Camera>(true);
-            previewCamera.targetTexture.width = 512*InfiniteFriends.MaxPlayerHardCap;
-            previewCamera.rect = new Rect(0f, 0f, InfiniteFriends.MaxPlayerHardCap, 1f);
-            previewCamera.pixelRect = new Rect(0f, 0f, 512f*InfiniteFriends.MaxPlayerHardCap, 512f);
-
-            // Extend the number of customizers and animators
-            while (___previewCustomizers.Count < InfiniteFriends.MaxPlayerHardCap)
-            {
-                // Generate preview transform and customizer
-                GameObject playerPreview = GameObject.Instantiate(___previewCustomizers[0].transform.parent.gameObject, ___previewCustomizers[0].transform.parent.parent);
-                SpiderCustomizer customizer = playerPreview.GetComponentInChildren<SpiderCustomizer>(true);
-                playerPreview.name = $"PlayerPreview {___previewCustomizers.Count+1}";
-                ___previewCustomizers.Add(customizer);
-
-                // Add animator
-                Animator animator = customizer.GetComponentInChildren<Animator>(true);
-                ___previewAnimators.Add(animator);
-            }
-
-            // Evenly space preview spiders for texture atlasing
-            const float previewWidth = 22f;
-            Vector3 leftmost = ___previewCustomizers[0].transform.parent.position;
-            leftmost.x = (InfiniteFriends.MaxPlayerHardCap-1) * (previewWidth/2f) * -1f;
-
-            for (int i = 0; i < ___previewCustomizers.Count; i++)
-            {
-                Transform playerPreview = ___previewCustomizers[i].transform.parent;
-                playerPreview.position = leftmost + new Vector3(previewWidth*i, 0f, 0f);
-            }
-
             return true;
         }
     }
---
+++ InfiniteFriends/Patches/CustomizationPreviewManager.cs
@@ -0,0 +1,51 @@
+using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InfiniteFriends.Patches
+{
+    /*
+     * Statically increase the number of preview spiders.
+     */
+
+    [HarmonyPatch(typeof(CustomizationPreviewManager), "Awake")]
+    class CustomizationPreviewManager_Patch_Awake
+    {
+        [HarmonyPrefix]
+        static bool Prefix(ref List<SpiderCustomizer> ___previewCustomizers, ref List<Animator> ___previewAnimators)
+        {
+            // Widen texture atlas
+            Camera previewCamera = ___previewCustomizers[0].transform.parent.parent.GetComponentInChildren<Camera>(true);
+            previewCamera.targetTexture.width = 512*InfiniteFriends.MaxPlayerHardCap;
+            previewCamera.rect = new Rect(0f, 0f, InfiniteFriends.MaxPlayerHardCap, 1f);
+            previewCamera.pixelRect = new Rect(0f, 0f, 512f*InfiniteFriends.MaxPlayerHardCap, 512f);
+
+            // Extend the number of customizers and animators
+            while (___previewCustomizers.Count < InfiniteFriends.MaxPlayerHardCap)
+            {
+                // Generate preview transform and customizer
+                GameObject playerPreview = GameObject.Instantiate(___previewCustomizers[0].transform.parent.gameObject, ___previewCustomizers[0].transform.parent.parent);
+                SpiderCustomizer customizer = playerPreview.GetComponentInChildren<SpiderCustomizer>(true);
+                playerPreview.name = $"PlayerPreview {___previewCustomizers.Count+1}";
+                ___previewCustomizers.Add(customizer);
+
+                // Add animator
+                Animator animator = customizer.GetComponentInChildren<Animator>(true);
+                ___previewAnimators.Add(animator);
+            }
+
+            // Evenly space preview spiders for texture atlasing
+            const float previewWidth = 22f;
+            Vector3 leftmost = ___previewCustomizers[0].transform.parent.position;
+            leftmost.x = (InfiniteFriends.MaxPlayerHardCap-1) * (previewWidth/2f) * -1f;
+
+            for (int i = 0; i < ___previewCustomizers.Count; i++)
+            {
+                Transform playerPreview = ___previewCustomizers[i].transform.parent;
+                playerPreview.position = leftmost + new Vector3(previewWidth*i, 0f, 0f);
+            }
+
+            return true;
+        }
+    }
+}
